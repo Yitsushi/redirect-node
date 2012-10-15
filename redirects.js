39b35028ca96724288af7644eae88b3dd9bbc184
Balazs Nadasdi <yitsushi@gmail.com>
2012-10-15 08:16:55 +0200

    dev.folyam.info

--- redirects.js
+++ redirects.js
@@ -1,6 +1,7 @@
 module.exports.domain = {
   'localhost'            : 'http://google.com',
   'plus.folyam.info'     : 'https://plus.google.com/u/0/105853491239853198987',
+  'dev.folyam.info'      : 'https://plus.google.com/u/0/105613682641367710983',
   'hatvanimagnosklub.hu' : 'http://www.hatvanimagnosklub.hu',
   'plus.dirtywindows.hu' : 'https://plus.google.com/113237392793237478295',
   'plus.blakedashed.org' : 'https://plus.google.com/108438016412516478289'
